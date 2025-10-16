f4e5dd31731816f68d0c35683868652ea31d5806
iDarkQ <matikuki3@gmail.com>
2025-10-16 20:51:24 +0100

    refactor(page.tsx): replace history with process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { About } from "@/app/components/home/about/about";
 import { Contact } from "@/app/components/home/contact/contact";
 import { Hero } from "@/app/components/home/hero/hero";
-import { History } from "@/app/components/home/history/history";
+import { Process } from "@/app/components/home/process/process";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
-      <History />
+      <Process />
       <Contact />
     </>
   );
