e7e7f48c3c9dc0bf124fd3ac9921297c1a65ebc1
iDarkQ <matikuki3@gmail.com>
2025-10-17 20:26:57 +0100

    fix(contact-form-button.tsx): wrap button in div to add animation (for some reason aos doesnt work with buttons)

--- src/app/components/home/contact/contact-form/contact-form-button.tsx
+++ src/app/components/home/contact/contact-form/contact-form-button.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/app/components/button";
 
 export const ContactFormButton = () => (
-  <Button data-aos="fade-up" filled={false} itemType="submit">
-    Send!
-  </Button>
+  <div data-aos="fade-up">
+    <Button filled={false} itemType="submit">
+      Send!
+    </Button>
+  </div>
 );
