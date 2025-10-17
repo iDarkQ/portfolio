5af8315aa26e5a27a0c65df3b36bb6a5d98aec65
iDarkQ <matikuki3@gmail.com>
2025-10-17 20:34:23 +0100

    feat(text.tsx): add h3 and h4

--- src/app/components/text.tsx
+++ src/app/components/text.tsx
@@ -2,16 +2,14 @@ import clsx from "clsx";
 import { ReactNode } from "react";
 
 interface Props {
-  as?: "p" | "h1" | "h2";
+  as?: "p" | "h1" | "h2" | "h3" | "h4";
   children: ReactNode;
   className?: string;
 }
 
 const baseHeading = "text-[var(--color-primary)] font-[475] leading-[0.75]";
 const styles = {
-  p: clsx(
-    "text-white paragraph",
-  ),
+  p: clsx("text-white paragraph"),
 
   h1: clsx(
     `text-8xl ${baseHeading}`, // normal
@@ -21,14 +19,20 @@ const styles = {
     `text-5xl ${baseHeading}`, // normal
     "max-xs:text-2xl max-sm:text-3xl max-lg:text-4xl"
   ),
+  h3: clsx(
+    `text-3xl ${baseHeading}`, // normal
+    "max-xs:text-lt max-sm:text-xl max-lg:text-2xl"
+  ),
+  h4: clsx(
+    `text-2xl ${baseHeading}`, // normal
+    "max-xs:text-md max-sm:text-lg max-lg:text-xl"
+  ),
 };
 
 export const Text = ({ as = "p", children, className }: Props) => {
   const Component = as;
   return (
-    <Component
-      className={clsx(styles[as], className && className)}
-    >
+    <Component className={clsx(styles[as], className && className)}>
       {children}
     </Component>
   );
