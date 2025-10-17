4557a4862bc86122525d35ddb7427fdf65e1f459
iDarkQ <matikuki3@gmail.com>
2025-10-17 20:29:36 +0100

    hero-highlights.tsx): reduce padding when on large smaller device

--- src/app/components/home/hero/hero-highlights.tsx
+++ src/app/components/home/hero/hero-highlights.tsx
@@ -1,7 +1,7 @@
 import { HeroHighlightMessage } from "@/app/components/home/hero/hero-highlight-message";
 
 export const HeroHighlights = () => (
-  <div className="flex flex-row gap-4 pt-10 max-md:pt-5">
+  <div className="flex flex-row gap-4 max-lg:pt-10 max-md:pt-5">
     <HeroHighlightMessage>
       Always focused at building scalable full stack solutions grounded in best
       practices at their core.
