d3b6810a396724636acfb9ecfbc9de77033a0793
iDarkQ <matikuki3@gmail.com>
2025-10-17 20:28:53 +0100

    refactor(contact.tsx): add contact id to the section

--- src/app/components/home/contact/contact.tsx
+++ src/app/components/home/contact/contact.tsx
@@ -2,7 +2,7 @@ import { Section } from "@/app/components/section";
 import { ContactDescription, ContactForm, ContactHeading } from ".";
 
 export const Contact = () => (
-  <Section extraClassName="gap-5">
+  <Section id="contact" extraClassName="gap-5">
     <ContactHeading />
     <ContactDescription />
     <ContactForm />
