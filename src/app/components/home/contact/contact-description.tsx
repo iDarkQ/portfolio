1277193a24f9c73b6128b9266a0ee661ee58c605
iDarkQ <matikuki3@gmail.com>
2025-10-17 20:28:36 +0100

    fix(home): remove exclemation mark after lg-paragraph

--- src/app/components/home/contact/contact-description.tsx
+++ src/app/components/home/contact/contact-description.tsx
@@ -1,7 +1,7 @@
 import { Text } from "@/app/components/text";
 
 export const ContactDescription = () => (
-  <Text data-aos="flip-up" className="lg-paragraph! text-center">
+  <Text data-aos="flip-up" className="lg-paragraph text-center">
     Got a question or proposal, or just want to say hello? Go ahead.
   </Text>
 );
--- src/app/components/home/hero/hero-message.tsx
+++ src/app/components/home/hero/hero-message.tsx
@@ -1,7 +1,7 @@
 import { Text } from "@/app/components/text";
 
 export const HeroMessage = () => (
-  <Text as="p" className="lg-paragraph!">
+  <Text as="p" className="lg-paragraph">
     I craft reliable full stack solutions, combining clean frontend design with
     efficient backend systems
   </Text>
