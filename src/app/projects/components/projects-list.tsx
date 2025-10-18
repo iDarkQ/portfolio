975015fd6d76ab3bcc9e0035f8ce20301a3aaf18
iDarkQ <matikuki3@gmail.com>
2025-10-18 21:31:53 +0100

    refactor(projects): move proejct-data.ts from utils to data folder

--- src/app/projects/components/projects-list.tsx
+++ src/app/projects/components/projects-list.tsx
@@ -1,5 +1,5 @@
 import { ProjectCard } from "@/app/components/project-card";
-import { projects } from "@/app/projects/utils/project-data";
+import { projects } from "@/app/projects/data/project-data";
 
 export const ProjectsList = () => (
   <div className="grid grid-cols-2 w-full gap-5 h-auto max-md:grid-cols-1">
