5814acfdf5d5f7ae38cacc2034248e74fc7db1e0
iDarkQ <matikuki3@gmail.com>
2025-10-19 09:08:00 +0100

    refactor: improve font loading speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,18 @@ import "@/assets/styles/global.css";
 import type { Metadata } from "next";
 import { AOSInit } from "@/app/components/aos";
 import { LayoutHeader, LayoutFooter } from "@/app/components/layout";
+import { Analytics } from "@vercel/analytics/next";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "iDarkQ",
   description:
     "Hi, I'm iDarkQ. Here you'll find my portfolio, projects, and resume ~ a mix of code and design.",
 };
+
+const googleFont = localFont({
+  src: "../assets/fonts/GoogleSansFlex.ttf",
+});
 
 export default function RootLayout({
   children,
@@ -18,7 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AOSInit />
-      <body className="relative">
+      <Analytics />
+      <body className={`relative ${googleFont.className}`}>
         <LayoutHeader />
         <main>{children}</main>
         <LayoutFooter />
