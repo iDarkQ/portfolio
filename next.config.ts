a54ce3ec88530eabd06095521c7190258af36480
iDarkQ <matikuki3@gmail.com>
2025-10-20 07:49:10 +0100

    refactor(next.config.ts): 404 page redirect  to home

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+    ];
+  }
 };
 
 export default nextConfig;
