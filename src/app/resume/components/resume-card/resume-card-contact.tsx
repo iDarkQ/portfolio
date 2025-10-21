901cedde334959e2ac31c1fe884eef6aa8a7a575
iDarkQ <matikuki3@gmail.com>
2025-10-21 19:00:11 +0100

    refactor(resume-card-contact.tsx): fix www link

--- src/app/resume/components/resume-card/resume-card-contact.tsx
+++ src/app/resume/components/resume-card/resume-card-contact.tsx
@@ -3,12 +3,12 @@ import { ResumeCardLink } from ".";
 
 export const ResumeCardContact = () => (
   <section className="flex flex-col gap-1">
-    <ResumeCardLink link="https://idarkq.dev/">idarkq.dev</ResumeCardLink>
+    <ResumeCardLink link="https://idarkq.dev/">https://idarkq.dev</ResumeCardLink>
     <ResumeCardLink link="mailto:idarkq.dev@gmail.com">
       idarkq.dev@gmail.com
     </ResumeCardLink>
     <ResumeCardLink link="https://github.com/iDarkQ">
-      github.com/iDarkQ
+      https://github.com/iDarkQ
     </ResumeCardLink>
     <ResumeCardLink link="tel:+351927402792">+351 927 402 792</ResumeCardLink>
     <Text>Coimbra, Portugal</Text>
