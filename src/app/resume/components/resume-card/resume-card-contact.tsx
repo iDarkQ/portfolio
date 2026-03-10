f370fd4bbc003edd323f3d49986f2ca55bc01ee5
iDarkQ <matikuki3@gmail.com>
2026-03-10 21:49:42 +0000

    feat(resume-card-contact.tsx): add linkedin

--- src/app/resume/components/resume-card/resume-card-contact.tsx
+++ src/app/resume/components/resume-card/resume-card-contact.tsx
@@ -3,14 +3,19 @@ import { ResumeCardLink } from ".";
 
 export const ResumeCardContact = () => (
   <section className="flex flex-col gap-1">
-    <ResumeCardLink link="https://idarkq.dev/">https://idarkq.dev</ResumeCardLink>
+    <ResumeCardLink link="https://idarkq.dev/">
+      https://idarkq.dev
+    </ResumeCardLink>
     <ResumeCardLink link="mailto:idarkq.dev@gmail.com">
       idarkq.dev@gmail.com
     </ResumeCardLink>
     <ResumeCardLink link="https://github.com/iDarkQ">
-      https://github.com/iDarkQ
+      https://github.com/idarkq
     </ResumeCardLink>
     <ResumeCardLink link="tel:+351927402792">+351 927 402 792</ResumeCardLink>
+    <ResumeCardLink link="https://www.linkedin.com/in/idarkq/">
+      linkedin.com/in/idarkq
+    </ResumeCardLink>
     <Text>Coimbra, Portugal</Text>
   </section>
 );
